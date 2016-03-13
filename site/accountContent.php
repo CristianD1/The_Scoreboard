1b50a360fe5512730e3dec3606f9600a8827fac3
root <Cristian.David909@gmail.com>
2016-03-12 23:09:41 -0500

    Added account registration and minor error checking

--- site/accountContent.php
+++ site/accountContent.php
@@ -13,9 +13,102 @@
   ?>
 
 
+    <div id="createAccount" class="menuItemContent">
+      <center><h4>Account Login/Signup</h4></center>
+      <ul class="collapsible" data-collapsible="accordion">
+        <li>
+          <div class="collapsible-header"><i class="material-icons">perm_identity</i>Create Account</div>
+          <div class="collapsible-body">
+            <div class="row">
+
+              <form id="registerAccountForm" class="col s12" action="php/registerAccount.php" method="POST"> <!-- Register account -->
+                <div class="row">
+                  <div class="input-field col s6">
+                    <i class="material-icons prefix">account_circle</i>
+                    <input name="firstName" id="firstName" type="text" class="validate">
+                    <label for="firstName">First Name (*)</label>
+                  </div>
+                  <div class="input-field col s6">
+                    <input name="lastName" id="lastName" type="text" class="validate">
+                    <label for="lastName">Last Name (*)</label>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="input-field col s12">
+                    <i class="material-icons prefix">note_add</i>
+                    <input name="aboutMe" id="aboutMe" type="text" class="validate">
+                    <label for="aboutMe">About me (optional)</label>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="input-field col s12">
+                    <i class="material-icons prefix">lock</i>
+                    <input name="securityCode" id="securityCode" type="text" class="validate">
+                    <label for="securityCode">Password (*)</label>
+                  </div>
+                </div>
+                <center><a onclick="validateForm('register')" class="waves-effect waves-light btn">Submit</a></center>
+              </form>
+
+            </div>
+          </div>
+        </li>
+        <li>
+          <div class="collapsible-header"><i class="material-icons">perm_identity</i>Sign In</div>
+          <div class="collapsible-body">
+            <div class="row">
+
+              <form id="loginForm" class="col s12"> <!-- sign in -->
+                <div class="row">
+                  <div class="input-field col s12">
+                    <i class="material-icons prefix">lock</i>
+                    <input id="login_securityCode" type="text" class="validate">
+                    <label for="login_securityCode">Password</label>
+                  </div>
+                </div>
+                <center><a onclick="validateForm('signIn')" class="waves-effect waves-light btn">Submit</a></center>
+              </form>
+
+            </div>
+          </div>
+          </div>
+      </ul>
+      <h4 id="accountError" style="color: red;"></h4>
+    </div>
+
+    <script>
+
+      $(function(){
+        // bleh. warnings and stuff.
+        Materialize.toast("If you got sent back here, try a different password.", 2000, 'rounded #ff5722 deep-orange');
+      })
+
+      // ez pz form validation. 
+      // Feel free to delete this, the php file will still reject you.
+      function validateForm(formName) { 
+        if ( formName === 'register' ) {
+          if ( $('#firstName').val() == "" || $('#lastName').val() == "" || $('#securityCode').val() == "" ) {
+            Materialize.toast("uh... you didn't fill in the form...", 2000, 'rounded #ff5722 deep-orange');
+          }else{
+            $('#registerAccountForm').submit();
+          }
+        } else if ( formName === 'signIn' ) {
+          if ( $('#login_securityCode').val() == "" ) {
+            Materialize.toast('Sign in with no password... Funny.', 2000, 'rounded #ff5722 deep-orange');
+          }else{
+            $('#loginForm').submit();
+          }
+        }
+      }
+
+    </script>
+
   <?php
     } else { // Show account page
   ?>
+
+
+
 
 
   <?php
---
+++ site/php/registerAccount.php
@@ -0,0 +1,40 @@
+<?php
+
+include 'conn.php';
+
+$db = new Db();
+
+$firstName = $_POST["firstName"];
+$lastName = $_POST["lastName"];
+$aboutMe = $_POST["aboutMe"];
+$securityCode = $_POST["securityCode"];
+
+if ( $firstName && $lastName && $securityCode ) {
+  echo $firstName . " " . $lastName . " " . $securityCode;
+
+  $sqlfirstName = htmlspecialchars($db -> quote($firstName));
+  $sqllastName = htmlspecialchars($db -> quote($lastName));
+  $sqlaboutMe = htmlspecialchars($db -> quote($aboutMe));
+  $sqlsecurityCode = htmlspecialchars($securityCode);
+  $hashedCode = $db -> quote(htmlspecialchars(hash("sha512", $sqlsecurityCode)));
+
+  echo "Starting sql";
+
+  // Ensure duplicate person doesn't exist
+  $checkDupes = $db -> select("SELECT Count(PersonID) as dupeCount FROM Persons WHERE SecurityCode=" . $hashedCode . ";");
+
+  if($checkDupes[0]['dupeCount'] == 0){
+    $result = $db -> query("INSERT INTO Persons (LastName, FirstName, SecurityCode, AboutMe) VALUES (" . $sqllastName . "," . $sqlfirstName . "," . $hashedCode . "," . $sqlaboutMe . ");");
+
+    echo "success";
+  }else{
+    echo "dupe";
+    header('Location: ../accountContent.php');
+  }
+
+} else {
+  echo "One or more necessary fields left blank.";
+  header('Location: ../accountContent.php');
+}
+
+?>
