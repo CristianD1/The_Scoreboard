022c0faa7b6c1168816f18a1b36a044b3035bc88
Cristian David <cristian.david909@gmail.com>
2016-04-07 20:09:54 -0400

    Adding product Pics

--- site/php/registerAccount.php
+++ site/php/registerAccount.php
@@ -14,9 +14,9 @@ $defaultElo = 1000;
 if ( $firstName && $lastName && $securityCode ) {
   echo $firstName . " " . $lastName . " " . $securityCode;
 
-  $sqlfirstName = htmlspecialchars($db -> quote($firstName), ENT_QUOTES);
-  $sqllastName = htmlspecialchars($db -> quote($lastName), ENT_QUOTES);
-  $sqlaboutMe = htmlspecialchars($db -> quote($aboutMe), ENT_QUOTES);
+  $sqlfirstName = htmlspecialchars($db -> quote($firstName));
+  $sqllastName = htmlspecialchars($db -> quote($lastName));
+  $sqlaboutMe = htmlspecialchars($db -> quote($aboutMe));
   $sqlsecurityCode = $securityCode;
   $hashedCode = $db -> quote( hash("sha512", $sqlsecurityCode));
 
